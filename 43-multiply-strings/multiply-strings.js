59f84397b8083581eb439372b9e60d11b979d03f
Manimaran_k <manimani58571@gmail.com>
2026-07-25 10:40:23 +0530

    Time: 9 ms (26.61%) | Memory: 55.8 MB (58.06%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function(num1, num2) {
+    if (num1 === "0" || num2 === "0") return "0";
+
+    const m = num1.length;
+    const n = num2.length;
+    const result = new Array(m + n).fill(0);
+
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+            const mul = Number(num1[i]) * Number(num2[j]);
+            const p1 = i + j;
+            const p2 = i + j + 1;
+            
+            const sum = mul + result[p2];
+
+            result[p2] = sum % 10;
+            result[p1] += Math.floor(sum / 10);
+        }
+    }
+
+   
+    while (result[0] === 0) {
+        result.shift();
+    }
+
+    return result.join("");
+};
