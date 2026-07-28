6cf98d635d156740dbd6334641d7cbd53c3e5796
Manimaran_k <manimani58571@gmail.com>
2026-07-28 11:11:47 +0530

    Time: 0 ms (100.00%) | Memory: 59.3 MB (47.14%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function(nums) {
+    const result = [];
+    const used = new Array(nums.length).fill(false);
+
+    nums.sort((a, b) => a - b);
+
+    function backtrack(current) {
+        if (current.length === nums.length) {
+            result.push([...current]);
+            return;
+        }
+
+        for (let i = 0; i < nums.length; i++) {
+           
+            if (used[i]) continue;
+
+           
+            if (i > 0 && nums[i] === nums[i - 1] && !used[i - 1]) continue;
+
+          
+            used[i] = true;
+            current.push(nums[i]);
+
+           
+            backtrack(current);
+
+           
+            current.pop();
+            used[i] = false;
+        }
+    }
+
+    backtrack([]);
+    return result;
+};
