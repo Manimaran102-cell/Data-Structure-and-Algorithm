c7da07557227592b59e4162b06d9d8adfb19a4b9
Manimaran_k <manimani58571@gmail.com>
2026-08-03 11:31:08 +0530

    Time: 0 ms (100.00%) | Memory: 57.6 MB (5.47%) - LeetSync

---
+++ 78-subsets/subsets.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+  const result = [[]];
+
+  for(const num of nums){
+    const size = result.length;
+    for(let i =0; i < size; i++){
+        result.push([...result[i], num]);
+    }
+  }
+  return result;  
+};
