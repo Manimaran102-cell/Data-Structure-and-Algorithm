78419799f4b14a07557591cd4aaac015ff70a384
Manimaran_k <manimani58571@gmail.com>
2026-08-03 11:48:18 +0530

    Time: 2 ms (9.66%) | Memory: 56 MB (5.06%) - LeetSync

---
+++ 75-sort-colors/sort-colors.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let len = nums.length;
+
+    for(let i = 0; i < len; i++){
+        for(let j = 0; j < len - 1 - i; j++){
+            if(nums[j] > nums[j + 1]){
+                [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]]
+            }
+        }
+    }
+    return nums;
+};
